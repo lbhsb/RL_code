d7fe2758a97d4eb52b461497847ffd7b7381ea91
lbhsb <55967145+lbhsb@users.noreply.github.com>
2021-11-18 21:01:30 +0800

    Update noisynet_DQN.py

--- noisynet_DQN.py
+++ noisynet_DQN.py
@@ -14,7 +14,7 @@ from tensorboardX import SummaryWriter
 
 env_id = "Enduro-ram-v0"
 env = gym.make(env_id).unwrapped
-writer = SummaryWriter('logs_1/noisy_dqn')
+writer = SummaryWriter('logs/noisy_dqn')
 USE_CUDA = torch.cuda.is_available()
 Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
 
